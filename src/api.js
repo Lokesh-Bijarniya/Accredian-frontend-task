7ad18a0b9a80fe0f6fdc95ff6d57c38ac16086d9
Lokesh_Bijarniya_ <152880035+Lokesh-Bijarniya@users.noreply.github.com>
2025-02-16 20:49:40 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000/api', 
+  baseURL: 'https://accredian-backend-task-42x9.onrender.com/api', 
   headers: {
     'Content-Type': 'application/json',
   },
